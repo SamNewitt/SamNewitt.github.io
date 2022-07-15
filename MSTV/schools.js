dced7cf0a731eac46727b046a38edb0fdda6e643
samnewitt <samuel.newitt@gmail.com>
2022-07-15 00:52:16 -0500

    Finished homepage including schools js

---
+++ MSTV/schools.js
@@ -0,0 +1,30 @@
+var schools=[
+"Your Favorite Siouxland Schools",
+"West Sioux (IA)",
+"Dakota Valley (SD)",
+"Sioux City East (IA)",
+"Harrisburg (SD)",
+"Hinton (IA)",
+"Sioux Falls Jefferson (SD)",
+"Sioux Falls Lincoln (SD)",
+"Sioux City North (IA)",
+"O'Gorman (SD)",
+"Sioux County (IA)",
+"Sioux Falls Roosevelt (SD)",
+"Sergeant Bluff - Luton (IA)",
+"Sioux City West (IA)",
+"Sioux Falls Washington (SD)",
+"The Central Iowa Metro League",
+
+];
+var timer;
+function school(param){
+  clearTimeout(timer);
+  document.getElementById("schools-title").style.opacity=0;
+timer=setTimeout(function(){
+      document.getElementById("schools-title").innerHTML="Covering "+schools[param];
+      document.getElementById("schools-title").style.opacity=1;
+
+  },125);
+
+}
