77df06919a03951b804b59476155c8459aa382b2
samnewitt <samuel.newitt@gmail.com>
2022-07-15 13:55:58 -0500

    Re arranged Schools

--- MSTV/schools.js
+++ MSTV/schools.js
@@ -1,20 +1,22 @@
 var schools=[
 "Your Favorite Siouxland Schools",
-"West Sioux (IA)",
 "Dakota Valley (SD)",
 "Sioux City East (IA)",
-"Harrisburg (SD)",
-"Hinton (IA)",
-"Sioux Falls Jefferson (SD)",
-"Sioux Falls Lincoln (SD)",
 "Sioux City North (IA)",
-"O'Gorman (SD)",
-"Sioux Center (IA)",
-"Sioux Falls Roosevelt (SD)",
 "Sergeant Bluff - Luton (IA)",
 "Sioux City West (IA)",
+"West Sioux (IA)",
+"Hinton (IA)",
+"Sioux Center (IA)",
+"Harrisburg (SD)",
+"Sioux Falls Jefferson (SD)",
+"Sioux Falls Lincoln (SD)",
+"O'Gorman (SD)",
+"Sioux Falls Roosevelt (SD)",
 "Sioux Falls Washington (SD)",
 "The Central Iowa Metro League",
+
+
 
 ];
 var timer;
