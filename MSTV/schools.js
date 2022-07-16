cfe46167f379ea2bc1aa9be08f6013d1a98e7c01
samnewitt <samuel.newitt@gmail.com>
2022-07-16 13:42:08 -0500

    Added BV

--- MSTV/schools.js
+++ MSTV/schools.js
@@ -5,16 +5,18 @@ var schools=[
 "Sioux City North (IA)",
 "Sergeant Bluff - Luton (IA)",
 "Sioux City West (IA)",
+"The Central Iowa Metro League",
 "West Sioux (IA)",
 "Hinton (IA)",
 "Sioux Center (IA)",
+"Brandon Valley (SD)",
 "Harrisburg (SD)",
 "Sioux Falls Jefferson (SD)",
 "Sioux Falls Lincoln (SD)",
 "O'Gorman (SD)",
 "Sioux Falls Roosevelt (SD)",
-"Sioux Falls Washington (SD)",
-"The Central Iowa Metro League",
+"Sioux Falls Washington (SD)"
+
 
 
 
