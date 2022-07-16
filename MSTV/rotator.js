d49c8519ea3500293791e83b160d2ef1745687a8
samnewitt <samuel.newitt@gmail.com>
2022-07-15 21:53:13 -0500

    finished mobile css for home page

--- MSTV/rotator.js
+++ MSTV/rotator.js
@@ -6,17 +6,14 @@ var index=0;
 var timer;
 goToSlide(0);
 function goToSlide (param){
-
   clearTimeout(timer);
 
- dots[index].style.height="30px";
-dots[index].style.width="30px";
+
 dots[index].style.backgroundColor="#bbb";
 dots[index].style.margin="10px";
 
 
-dots[param].style.height="40px";
-dots[param].style.width="40px";
+
 dots[param].style.backgroundColor="#17478e";
 dots[param].style.margin="5px";
 
@@ -24,6 +21,19 @@ rotator.style.backgroundImage=images[param];
 pages[index].style.opacity="0";
 pages[param].style.opacity="0";
 
+if(Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)<700)
+{
+  dots[index].style.height="15px";
+  dots[index].style.width="15px";
+  dots[param].style.height="25px";
+  dots[param].style.width="25px";
+}
+else{
+dots[index].style.height="30px";
+dots[index].style.width="30px";
+dots[param].style.height="40px";
+dots[param].style.width="40px";
+}
 
 setTimeout(function(){
 pages[index].style.display="none";
